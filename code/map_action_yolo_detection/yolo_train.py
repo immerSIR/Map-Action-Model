edd8bd498997044c573b0a721fdca87e0437d3ac
yugo19 <doucouremohamed8@gmail.com>
2023-11-30 10:07:37 +0000

    added yolo_model_folder

--- code/map_action_yolo_detection/yolo_train.py
+++ code/map_action_yolo_detection/yolo_train.py
@@ -7,5 +7,5 @@ model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 model.train(data="data.yaml", epochs=20)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("/home/yugo19/Documents/Exp-data/yolo-data/images/dumpgarbage103_jpeg.rf.fe62527242b552a1d2ad0f12f83dacd4.jpg")  # predict on an image
+results = model("")  # predict on an image
 path = model.export(format="onnx")
